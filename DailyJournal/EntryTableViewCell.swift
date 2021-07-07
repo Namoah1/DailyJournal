86243939b3812e898b987ccdb34a0c2fe9611d47
Namoah1 <namoah@macalester.edu>
2021-07-07 00:41:58 -0500

    TableVC file Added

--- DailyJournal/EntryTableViewCell.swift
+++ DailyJournal/EntryTableViewCell.swift
@@ -11,7 +11,7 @@ class EntryTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Initialization code l
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
